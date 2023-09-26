9d79a932cbef6d125d7d2842dece98b771fd9d67
Neil Green <neil.r.green@gmail.com>
2023-09-26 09:28:45 +0100

    remove error type just propagate existing error

--- src/asynchronous.rs
+++ src/asynchronous.rs
@@ -1,25 +1,23 @@
-use crate::{backoff::Backoff, Error};
+use crate::backoff::Backoff;
 use std::{future::Future, time::Duration};
 
 pub async fn retry<R, S, T, E>(
     mut backoff: impl Backoff,
     sleep: impl Fn(Duration) -> S,
     mut body: impl FnMut() -> R,
-) -> Result<T, Error<E>>
+) -> Result<T, E>
 where
     R: Future<Output = Result<T, E>>,
     S: Future,
 {
-    let mut tries = 0;
     loop {
         match body().await {
             Ok(t) => return Ok(t),
-            Err(source) => {
-                tries += 1;
+            Err(e) => {
                 if let Some(delay) = backoff.next_delay() {
                     sleep(delay).await;
                 } else {
-                    return Err(Error::new(tries, source));
+                    return Err(e);
                 }
             }
         }
